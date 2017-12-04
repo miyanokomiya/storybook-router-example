c663b5bbdae2ae3600bd7f74de1151dac8f90bf5
miyanokomiya <komiyamatomoya@mbp.local>
2017-12-05 08:37:37 +0900

    Create with and without story-router sample.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,17 @@
-import { configure } from '@storybook/vue';
+import { configure } from "@storybook/vue";
 
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import VueRouter from "vue-router";
 
-import MyButton from '../src/stories/Button.vue'
+import MyButton from "../src/stories/Button.vue";
 
-Vue.component('my-button', MyButton)
-Vue.use(Vuex)
+Vue.component("my-button", MyButton);
+Vue.use(Vuex);
+// Vue.use(VueRouter);
 
 function loadStories() {
-  require('../src/stories');
+  require("../src/stories");
 }
 
 configure(loadStories, module);
